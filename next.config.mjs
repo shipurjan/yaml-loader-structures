a9f95ac152ef34fb2225f73c5081346978f10b87
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-03-12 12:49:36 +0100

    create a demo of loaded yaml structures
    examples described in https://yaml.org/spec/1.2.2/#22-structures loaded with yaml-loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      oneOf: [
+        {
+          resourceQuery: /stream/,
+          options: { asStream: true },
+          loader: 'yaml-loader'
+        },
+        { loader: 'yaml-loader' }
+      ]
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
